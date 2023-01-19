edbc6d44cb3fcf58b7bbdcd07a314cb20de1d426
Jack Whelan <61770531+jw176@users.noreply.github.com>
2023-01-19 20:37:49 +1100

    Feature basic skeleton (#1)
    * Create server.py
    
    * Added .idea to the .gitignore
    
    * Added skeleton code for the game server
    
    * added custom exceptions for the game server
    
    * added a result class to store info about a single match outcome
    
    * Create requirements.txt
    
    * function for loading game paramters and constants
    
    * refactor folder structure and custom exceptions to new file
    
    * added basic logging for now
    
    * removed extra character in requirements file
    
    * added main.py
    
    Co-authored-by: Amir Toosi <amir.ghalb@gmail.com>

---
+++ server/main.py
@@ -0,0 +1,5 @@
+from .server import Game
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
---
+++ server/server.py
@@ -0,0 +1,61 @@
+import logging
+
+
+class GameState:
+    def __init__(self):
+        pass
+
+
+class Move:
+    def __init__(self):
+        # calls self._parse() and returns an exception if there is an error in the message received from the player
+        pass
+
+    def _parse(self):
+        pass
+
+
+class Player:
+    def __init__(self):
+        # setup communication channel with the player's bot
+        pass
+
+    def request_move(self):
+        # supply the relevant gamestate info to the player, then await their move
+        pass
+
+
+class Game:
+    def __init__(self):
+        # setup communication with the two(?) players
+        # initialise replay and logs
+        pass
+
+    def _initialise_state(self):
+        # initialise the map by either loading from a file or randomly generating one (handled by other methods)
+        # select spawn location for players, etc
+        pass
+
+    def _play_turn(self):
+        # request move from each player
+        # check if the moves are valid in the context of the gamestate (e.g. check bot has ammo if trying to shoot)
+        # play moves
+        # update the gamestate for a single tick (calculate collisions, bullet movements, closing map ring, etc)
+        # if the game is terminal, return the result
+        pass
+
+    def _is_terminal(self):
+        # check which players still have hp
+        # if both players have 0 hp, it is a draw. Else the player with 0 hp loses
+        pass
+
+    def run(self):
+        # initialise state
+        logging.info("Starting game between players: ... and ...")
+        # while true (or number of turns is less than max turns), play_move
+        pass
+
+
+class Result:
+    def __init__(self):
+        self.winning_player_id = None  # if =None, the game was a draw
