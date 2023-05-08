332595fcc1467645af0ac73e777d50fbe48d4705
Ali Toosi <alirezakhoshghalb@ymail.com>
2023-05-08 16:51:23 +1000

    Fix linting issues

--- src/gameObjects/__init__.py
+++ src/gameObjects/__init__.py
@@ -1,3 +1,3 @@
-from .bullet import Bullet
-from .tank import Tank
-from .wall import Wall
+from .bullet import Bullet  # noqa: F401
+from .tank import Tank  # noqa: F401
+from .wall import Wall  # noqa: F401
